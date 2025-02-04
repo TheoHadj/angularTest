84ccf894e6bf792d310f9b971376e21647762a1b
Theo Hadj <theo.hadja@gmail.com>
2025-02-04 14:41:41 +0100

    ngStyle

--- src/app/one-friend/one-friend.component.ts
+++ src/app/one-friend/one-friend.component.ts
@@ -1,17 +1,35 @@
 import { Component } from '@angular/core';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-one-friend',
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './one-friend.component.html',
   styleUrl: './one-friend.component.css'
 })
 export class OneFriendComponent {
 
   friend = { id: 1, name: 'Alice', age: 25, status: 'Online', bio: 'Aime le développement web', xss: "<script>alert('Hello, XSS')</script>" }
+  unAmiStatus = true;
+  constructor() { 
+    if(Math.random() < 0.5){
+      this.unAmiStatus = false;
+  }
+  else{
+    this.unAmiStatus = true;
 
+  }
+}
 
-  
+  getColor(){
+    if(this.unAmiStatus){
+      return 'green';
+    }
+    else{
+      return 'red';
+    }
+  }  
+
   getOneFriendStatus(){
     return this.friend.status;
   }
